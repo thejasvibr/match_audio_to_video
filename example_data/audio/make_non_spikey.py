da62d0de368d32b30b197caf3c24a26369f9a52c
thejasvibr <tbeleyur@orn.mpg.de>
2020-01-27 12:52:57 +0100

    version to make 2018-08-16 non-spikey on Windows. I seem to get some weird OSerror cannot allocate memory issues on my Ubuntu

--- example_data/audio/make_non_spikey.py
+++ example_data/audio/make_non_spikey.py
@@ -7,6 +7,7 @@ Created on Tue Aug 13 09:05:24 2019
 """
 import pdb
 import os
+import resource
 import sys 
 sys.path.append('../../bin/')
 import glob
@@ -16,21 +17,24 @@ import soundfile as sf
 import av_sync
 from audio_for_videoannotation import get_file_series
 
-#raw_audio_folder ='/media/tbeleyur/THEJASVI_DATA_BACKUP_3/fieldwork_2018_002/horseshoe_bat/audio/2018-08-16/'
-raw_audio_folder = '/home/thejasvi/audio_mtpt/'
+raw_audio_folder ='/media/tbeleyur/THEJASVI_DATA_BACKUP_3/fieldwork_2018_002/horseshoe_bat/audio/2018-08-16/'
+#raw_audio_folder = '/home/thejasvi/audio_mtpt/'
 all_wav_files = glob.glob(raw_audio_folder+'*.WAV')
 
-audiofiles = get_file_series(('674','744'), all_wav_files)
+audiofiles = get_file_series(('684','744'), all_wav_files)
 
 for each_file in audiofiles:
     print(each_file)
     audio, fs = sf.read(each_file)
     sync = audio[:,-1]
+    print('Memory usage: %s (kB)'%resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     reconstr_audio = av_sync.get_audio_sync_signal(sync, parallel=True,
                                            min_distance=int(fs*0.07*2))
-    pdb.set_trace()
+    print('Memory usage: %s (kB)'%resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     print('Done with reconstruction')
-    final_audio = np.column_stack((audio, reconstr_audio))
+    #final_audio = np.column_stack((audio, reconstr_audio))
     file_name = os.path.split(each_file)[-1]
-    sf.write('non_spikey_' + file_name, final_audio,
+    sf.write('non_spikey_' + file_name, reconstr_audio,
              samplerate=fs)
+    del audio, sync, reconstr_audio
+    print('Memory usage: %s (kB)'%resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
