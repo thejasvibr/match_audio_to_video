983d40e1d39022c4f53cc28046498968119ca6bd
thejasvibr <tbeleyur@orn.mpg.de>
2020-02-28 14:48:50 +0100

    audio matching for 2150-2300 2018-08-16 and also add optional AND/OR switch to read timestamp and/or lamp intensity

--- experimental_testdata/horseshoebat_data/whole_data_analysis/raw_sync/generate_videosync_for_videos.py
+++ experimental_testdata/horseshoebat_data/whole_data_analysis/raw_sync/generate_videosync_for_videos.py
@@ -18,21 +18,22 @@ import scipy.signal as signal
 
 from generate_data_from_video import generate_videodata_from_videofiles, segment_numbers_and_resize
 
-
-annotation_files = glob.glob('../annotations/'+'*.csv')
+video_annotation_files = '/home/tbeleyur/Documents/packages_dev/match_audio_to_video/experimental_testdata/horseshoebat_data/whole_data_analysis/'
+annotation_files = glob.glob(video_annotation_files+'annotations/'+'*.csv')
 print(annotation_files)
-file_index = 1
+file_index = 0
 annotations = pd.read_csv(annotation_files[file_index])
 print('processing...', annotation_files[file_index][-40:])   
 
-#STARTFRAME = 50000
+STARTFRAME = 32000
 
 generate_videodata_from_videofiles(annotations, 
                                    custom_processing=segment_numbers_and_resize,
                                    final_dims=(1400,200),
-                                   numeric_pixel_threshold=225,)
-#                                   start_frame=STARTFRAME,
-#                                   end_frame=STARTFRAME+100)
+                                   numeric_pixel_threshold=225,
+                                   read_timestamp_and_lamp=(False, True),
+                                   start_frame=STARTFRAME,
+                                   end_frame=STARTFRAME+100)
 #
 sync_files = glob.glob('*.csv')
 print(sync_files)
@@ -42,5 +43,6 @@ df = pd.read_csv(sync_files[0])
 #
 plt.figure()
 plt.plot(df['led_intensity'])
+plt.grid()
 plt.show()
 print(df['timestamp'])
